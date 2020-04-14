0cc5b0112f4b1c271e91f9909f26a338f6c65681
Earl Inis Mona <earl.inis.mona@protonmail.com>
2020-04-14 21:50:07 +0700

    Added .clang-format; formatted code according to it

--- src/main.c
+++ src/main.c
@@ -1,13 +1,10 @@
-#include <time.h>
-#include <stdio.h>
 #include "func.h"
+#include <stdio.h>
+#include <time.h>
 
 int main(int argc, const char** argv)
 {
-    int k;
-
-    k = check();
-
+    int k = check();
     printf("\nResult\t %d", k);
 
     return 0;
