3d34027d058ac0db336bae27c852838ff466da9a
Belstowe <earl.inis.mona@protonmail.com>
2020-05-30 18:13:19 +0700

    Применения clang-format к исходному коду

--- src/curses.h
+++ src/curses.h
@@ -1,10 +1,9 @@
 #ifndef Curs
 #define Curs
 
-    void Menu ( int *item);
-    void Difficult ( int *lvl);
-    void Play ( int *lvl, int *var);
-    void Variant ( int *var);
-
+void Menu(int* item);
+void Difficult(int* lvl);
+void Play(int* lvl, int* var);
+void Variant(int* var);
 
 #endif
--- src/func.h
+++ src/func.h
@@ -1,11 +1,11 @@
 #ifndef TheFunc
 #define TheFunc
-    int getrand(int min, int max);
-    //uint32_t* random_order(size_t num);
-    //void word_count(char* IFILE);
-    //void read_words(char* IFILE, Words* w);
-    //int sum(char* mass, int i, Words* w, int q);
-    void eng (int *lvl, int *var);
-    void rus (int *lvl, int *var);
+int getrand(int min, int max);
+// uint32_t* random_order(size_t num);
+// void word_count(char* IFILE);
+// void read_words(char* IFILE, Words* w);
+// int sum(char* mass, int i, Words* w, int q);
+void eng(int* lvl, int* var);
+void rus(int* lvl, int* var);
 
 #endif
