71deac2d23cd09940c066f629c4736912ef9f632
Belstowe <earl.inis.mona@protonmail.com>
2020-06-03 22:51:49 +0700

    Сделал поправки к "align_string", добавил в комплекс тестирования "ui_assist" тестирование данной функции

--- src/ui.c
+++ src/ui.c
@@ -36,28 +36,32 @@ void align_string(char* str, unsigned screen_width)
     char* token = str;
     unsigned index = 0;
 
-    while (token[index] != '\0') {
-        if (token[index] & 0x80) {      // 1*******
-            if (!(token[index] & 0x40)) // *0******
+    for (unsigned sym = 0; token[sym] != '\0'; sym++) {
+        if (token[sym] & 0x80) {        // 1*******
+            if (!(token[sym] & 0x40)) { // *0******
                 continue; // 10****** - байты, кодирующие символы размером более
-        } // 1 байта; оттого их считать не надо
+            } // 1 байта; оттого их считать не надо
+        }
 
-        if (token[index] == '\n') {
-            token = &(token[index + 1]);
-            last_space_position = index = 0;
+        if (token[sym] == '\n') {
+            token = &(token[sym]);
+            last_space_position = sym = index = 0;
             continue;
         }
 
-        if (token[index] == ' ')
-            last_space_position = index;
+        if (token[sym] == ' ')
+            last_space_position = sym;
 
         index++;
         if (index >= screen_width) {
-        	if (last_space_position != 0)
+        	if (last_space_position != 0) {
         		token[last_space_position] = '\n';
-
-            token = &(token[last_space_position + 1]);
-            last_space_position = index = 0;
+        		token = &(token[last_space_position]);
+        	}
+        	else
+        		token = &(token[sym]);
+            
+            last_space_position = sym = index = 0;
         }
     }
 }
